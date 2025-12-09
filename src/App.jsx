c7e40e36d19363e6a3a05abc9928bca677e47776
asotorod <asotorod@gmail.com>
2025-12-09 17:47:39 -0500

    Add IntroVideoModal to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import IntroVideoModal from './components/IntroVideoModal'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -13,20 +14,23 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="fleet" element={<Fleet />} />
-        <Route path="green" element={<Green />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="events" element={<Events />} />
-        <Route path="careers" element={<Careers />} />
-        <Route path="apply" element={<Apply />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+      <IntroVideoModal />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="fleet" element={<Fleet />} />
+          <Route path="green" element={<Green />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="events" element={<Events />} />
+          <Route path="careers" element={<Careers />} />
+          <Route path="apply" element={<Apply />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
